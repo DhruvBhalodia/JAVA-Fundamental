adacfeb9ab658d9459e26b6dca944c7114f2d589
DhruvBhalodia <dhruvcoder007@gmail.com>
2023-04-28 23:46:43 +0530

    Size of data types are added

--- DataTypes.java
+++ DataTypes.java
@@ -3,9 +3,13 @@ class DataTypes {
     public static void main(String[] args) {
         //Premitive Datatypes
         byte firstVariable = 127; //max size of byte datatype : 127 (2^7 - 1)
+        //byte : 1 byte
         short second = 32767; //max size of byte datatype : 32767 (2^15 - 1)
+        //short : 2 bytes
         int thrd = 2147483647; //max size of int datatype : 2147483647 (2^31 - 1)
+        //int : 4 bytes
         long frth = 9223372036854775807l; //max size of long datatype : 9223372036854775807 (2^63 - 1)
+        //long : 8 bytes
         //without l or L long stores only upto 2^31 - 1
         System.out.println(firstVariable);
         System.out.println(second);
