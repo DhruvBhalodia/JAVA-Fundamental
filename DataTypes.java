9a01e0f18de0ff653148c84d8607bb3487e478c2
DhruvBhalodia <dhruvcoder007@gmail.com>
2023-04-28 23:23:20 +0530

    Information about Data Types

---
+++ DataTypes.java
@@ -0,0 +1,19 @@
+class DataTypes {
+    //Premitive and Non Premitive
+    public static void main(String[] args) {
+        //Premitive Datatypes
+        byte firstVariable = 127; //max size of byte datatype : 127 (2^7 - 1)
+        short second = 32767; //max size of byte datatype : 32767 (2^15 - 1)
+        int thrd = 2147483647; //max size of int datatype : 2147483647 (2^31 - 1)
+        long frth = 9223372036854775807l; //max size of long datatype : 9223372036854775807 (2^63 - 1)
+        //without l or L long stores only upto 2^31 - 1
+        System.out.println(firstVariable);
+        System.out.println(second);
+        System.out.println(thrd);
+        System.out.println(frth);
+        float a = 45.12345f; //valid till 5 decimal points
+        double b = 45.12345678; //valid till 8 decimal points
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
