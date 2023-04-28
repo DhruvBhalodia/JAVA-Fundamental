665d2dbc78c8afb3c9c048c0b7918eea010c466b
DhruvBhalodia <dhruvcoder007@gmail.com>
2023-04-28 11:53:21 +0530

    Hello World programme in JAVA

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 class HelloWorld{ //Class name same as file name
     public static void main(String[] args) { //main method
+        //String[] args recive Strings on runtime
         System.out.println("Hello World"); //display "Hello World" in consol
     }
 }
